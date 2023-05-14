17a6b00c9d1f8df325c0566120b646c04710d3e6
loothfee <m.luthfifarid@gmail.com>
2023-05-15 06:54:23 +0700

    Deleted "Purge", move to "Content"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  content: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: true,
   theme: {
     extend: {},
