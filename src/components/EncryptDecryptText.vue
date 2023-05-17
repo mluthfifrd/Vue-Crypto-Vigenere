e0c88b5c3d103cace37fd9bcd9c8bfe5f4c866a7
loothfee <m.luthfifarid@gmail.com>
2023-05-17 13:26:05 +0700

    Update Button Components

--- src/components/EncryptDecryptText.vue
+++ src/components/EncryptDecryptText.vue
@@ -171,13 +171,13 @@ onMounted(() => {
             <div class="col-span-6">
               <button
                 @click.prevent="handleEncryptClick"
-                class="p-3 mr-3 bg-green-400 text-gray-900 hover:bg-green-500 rounded-md"
+                class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
               >
                 Enkripsi
               </button>
               <button
                 @click.prevent="handleDecryptClick"
-                class="p-3 bg-green-400 text-gray-900 hover:bg-green-500 rounded-md"
+                class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
               >
                 Dekripsi
               </button>
