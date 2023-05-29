328e80249d6963c2849613b4c20eebb6218dabec
loothfee <m.luthfifarid@gmail.com>
2023-05-29 15:07:17 +0700

    Update CSS

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@ header {
   margin: 0 auto 2rem;
 }
 
-nav {
+/* nav {
   width: 100%;
   font-size: 13px;
   text-align: center;
@@ -54,13 +54,14 @@ nav a {
 
 nav a:first-of-type {
   border: 0;
-}
+} */
 
 @media (min-width: 1024px) {
   header {
     display: flex;
+    flex-direction: column;
     place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    /* padding-right: calc(var(--section-gap) / 2); */
   }
 
   .logo {
@@ -73,13 +74,13 @@ nav a:first-of-type {
     flex-wrap: wrap;
   }
 
-  nav {
+  /* nav {
     text-align: left;
     margin-left: -1rem;
     font-size: 1rem;
 
     padding: 1rem 0;
     margin-top: 1rem;
-  }
+  } */
 }
 </style>
